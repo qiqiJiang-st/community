0dbe675c002df096fadeef3ee1d1cee02aa5b6c1
dell <jiangqiiq@163.com>
2022-07-14 11:05:34 +0800

    开发登录、退出功能

--- src/test/java/com/qqj/community/MapperTests.java
+++ src/test/java/com/qqj/community/MapperTests.java
@@ -1,8 +1,10 @@
 package com.qqj.community;
 
 import com.qqj.community.dao.DiscussPostMapper;
+import com.qqj.community.dao.LoginTicketMapper;
 import com.qqj.community.dao.UserMapper;
 import com.qqj.community.entity.DiscussPost;
+import com.qqj.community.entity.LoginTicket;
 import com.qqj.community.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,9 @@ public class MapperTests {
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
 
     @Test
     public void testSelectUser(){
@@ -69,5 +74,26 @@ public class MapperTests {
 
        int rows = discussPostMapper.selectDiscussPostRows(149);
         System.out.println(rows);
+    }
+
+    @Test
+    public void testInsertLoginTicket(){
+        LoginTicket loginTicket=new LoginTicket();
+        loginTicket.setUserId(101);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+    @Test
+    public void testSelectLoginTicket(){
+        LoginTicket loginTicket= loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+
+        loginTicketMapper.updateStatus("abc",1);
+        loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
     }
 }
