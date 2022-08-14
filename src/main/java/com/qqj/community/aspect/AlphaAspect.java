f05e7bd45330542472f5a330f4456730fb7b9748
dell <jiangqiiq@163.com>
2022-08-14 14:15:31 +0800

    统一记录日志

---
+++ src/main/java/com/qqj/community/aspect/AlphaAspect.java
@@ -0,0 +1,43 @@
+package com.qqj.community.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+//@Component
+//@Aspect
+public class AlphaAspect {
+    @Pointcut("execution(* com.qqj.community.service.*.*(..))")
+    public void pointcut(){
+
+    }
+
+    @Before("pointcut()")
+    public void before(){
+        System.out.println("before");
+    }
+
+    @After("pointcut()")
+    public void after(){
+        System.out.println("after");
+    }
+
+    @AfterReturning("pointcut()")
+    public void afterReturning(){
+        System.out.println("afterReturning");
+    }
+
+    @AfterThrowing("pointcut()")
+    public void afterThrowning(){
+        System.out.println("afterThrowning");
+    }
+
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable{
+        System.out.println("around before");
+        Object obj = joinPoint.proceed();
+        System.out.println("around after");
+        return obj;
+    }
+
+}
